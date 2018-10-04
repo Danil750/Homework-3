7dbe3d7c54afa82a0db193b611664f5f98ae7127
Danil750 <43548452+Danil750@users.noreply.github.com>
2018-10-04 23:01:30 +0300

    The task of finding the area of a circle done

--- CircleSquare.cs
+++ CircleSquare.cs
@@ -4,33 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace Training
 {
+    /* Задача 1.
+     Написать алгоритм, рассчитывающий площадь круга по заданному радиусу.
+     Примерный вид интерфейса:
+     ------------------------------------
+     Введите радиус круга:
+     Площадь круга: ...*/
+
     class Program
     {
-        /*Написать алгоритм, рассчитывающий площадь круга по заданному радиусу.
-        Примерный вид интерфейса:
-        ----------------------------------—
-        Введите радиус круга:
-        Площадь круга: ...*/
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите радиус круга: ");
-            string r = Console.ReadLine();
-            int radius;
-            
-            if (int.TryParse(r, out radius))
-            {
-                //S = pi * r * r
-                Console.WriteLine("Площадь круга: " + radius * radius + "pi");
-            }
-            else
-            {
-                Console.WriteLine("Неверный ввод");
-            }
-            
+            Console.Write("Введите радиус круга: ");
+            const double pi = 3.14;
+            string rString = Console.ReadLine();
+            double r;
+            double.TryParse(rString, out r);
+            Console.Write("Площадь круга: " + (pi * r * r));
             Console.ReadKey();
+
         }
     }
 }
